0db420f7348298cca3f305c10fe4027d9aa7cbbf
Gary Clark (OSV) <gary.clark.osv@fedex.com>
2025-07-06 02:05:15 +0200

    chore: more

--- home/programs/shell-tools.nix
+++ home/programs/shell-tools.nix
@@ -13,6 +13,25 @@
     poppler
     jq
     zip
+    _7zz
+    bash
+    gh
+    k9s
+    lua-language-server
+    uv
+    # telnet
+    # ping
+    terraform
+    terraform-ls
+    yaml-language-server
+    go-task
+    vscode-extensions.llvm-org.lldb-vscode
+    azure-cli
+    kubectl
+    docker
+    docker-compose
+    ruff
+    pre-commit
   ];
 
 }
