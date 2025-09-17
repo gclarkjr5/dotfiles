1e7e14f30dec1011e065b82343fcce0354d802ef
Gary Clark (OSV) <gary.clark.osv@fedex.com>
2025-09-18 00:29:29 +0200

    fix: build master

--- home/programs/gitui.nix
+++ home/programs/gitui.nix
@@ -8,17 +8,27 @@ let
     src = pkgs.fetchFromGitHub {
       owner = "extrawurst";
       repo = "gitui";
-      rev = "master"; # You can replace this with a specific commit for stability
+      rev = "f5893d991f66530dcefc48c9020ade77e33edfb9"; # You can replace this with a specific commit for stability
       fetchSubmodules = true;
       # ↓ Use `nix build` to get the right sha256 and replace this dummy
-      sha256 = "0000000000000000000000000000000000000000000000000000";
+      sha256 = "xV0x1oJ+PrGqaye9QJ1XJPj5pzzZdgukW+RYhTh2Pt8=";
+
     };
 
+    cargoLock = {
+      lockFile = "${src}/Cargo.lock";
+    };
     # ↓ You'll need to fill this in with the correct hash too
     cargoSha256 = "0000000000000000000000000000000000000000000000000000";
 
-    nativeBuildInputs = [ pkgs.pkg-config ];
+    nativeBuildInputs = with pkgs; [
+      git
+      cmake
+      perl
+      pkg-config
+    ];
     buildInputs = [ pkgs.openssl ];
+    doCheck = false;
   };
 in
 {
