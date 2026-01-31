65d031f760356fc281dd13b35a1c2d7e29f6960f
Gary Clark (OSV) <gary.clark.osv@fedex.com>
2026-01-31 16:29:42 +0100

    chore: attempt rust targets with bin

--- home/programs/rust.nix
+++ home/programs/rust.nix
@@ -10,11 +10,18 @@
 #   ];
 # }
 
+let
+  rustTargets = [
+    "aarch64-apple-darwin"
+    "aarch64-apple-ios-sim"
+  ];
+in
 {
   home.packages = with pkgs; [
-    rust-bin.stable."1.91.0".default
+    (rust-bin.stable."1.91.0".default.override { targets = rustTargets; })
+    (rust-bin.stable.latest.default.override { targets = rustTargets; })
+    (rust-bin.nightly.latest.default.override { targets = rustTargets; })
     rust-analyzer
     bacon
-    rustup
   ];
 }
