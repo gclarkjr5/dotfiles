7a658ff92371f11f37996e427f619bf465d98b6b
Gary Clark (OSV) <gary.clark.osv@fedex.com>
2026-02-09 13:18:29 +0100

    chore: xcc yazi install

--- home/programs/yazi.nix
+++ home/programs/yazi.nix
@@ -7,4 +7,5 @@
   home.file.".config/yazi/keymap.toml".source = ../config/yazi/keymap.toml;
   home.file.".config/yazi/theme.toml".source = ../config/yazi/theme.toml;
   home.file.".config/yazi/yazi.toml".source = ../config/yazi/yazi.toml;
+  home.file.".config/yazi/package.toml".source = ../config/yazi/package.toml;
 }
