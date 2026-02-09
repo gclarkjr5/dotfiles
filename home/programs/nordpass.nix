b2ef0de90d57e7c4a027fc1c7b6946599bff9ab2
Gary Clark (OSV) <gary.clark.osv@fedex.com>
2026-02-09 10:00:07 +0100

    chore: nordpass

--- home/programs/nordpass.nix
+++ home/programs/nordpass.nix
@@ -3,7 +3,7 @@
 let
   pkg = pkgs.fetchurl {
     url = "https://downloads.npass.app/mac/NordPass.dmg";
-    sha256 = "9cc719b0a0a60773b8ff08221b6a757314b19e12624bcf4510e65fc0e2cdb53d";
+    sha256 = "jKjB2RSAYkohN2mG7yRaJPdDa25YlLneYtkz+UF4ZF0=";
   };
 in
 {
