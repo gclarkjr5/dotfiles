e6a4d4f0ef11365725d9cd5a6f4ba5b999509019
Gary Clark <gclark@xccelerated.io>
2026-02-09 13:41:15 +0100

    chore: package toml yazi

--- home/programs/yazi.nix
+++ home/programs/yazi.nix
@@ -7,5 +7,14 @@
   home.file.".config/yazi/keymap.toml".source = ../config/yazi/keymap.toml;
   home.file.".config/yazi/theme.toml".source = ../config/yazi/theme.toml;
   home.file.".config/yazi/yazi.toml".source = ../config/yazi/yazi.toml;
-  home.file.".config/yazi/package.toml".source = ../config/yazi/package.toml;
+  home.file.".config/yazi/package.template.toml".source = ../config/yazi/package.template.toml;
+
+  # 🔧 Add a hook to install plugins after config is linked
+  home.activation.installYaziPlugins = config.lib.dag.entryAfter [ "writeBoundary" ] ''
+    if [ ! -f "$HOME/.config/yazi/package.toml" ]; then
+      cp "$HOME/.config/yazi/package.template.toml" "$HOME/.config/yazi/package.toml"
+      echo "[home.activation] Installing Yazi plugins..."
+      ${pkgs.yazi}/bin/ya pkg install
+    fi
+  '';
 }
