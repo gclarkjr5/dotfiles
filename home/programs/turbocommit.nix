e99efb690cd01475c42dc60901165e39d9bbcc1e
Gary Clark <gclark@xccelerated.io>
2026-02-28 21:41:12 +0100

    chore(turbocommit): add in nix build

--- home/programs/turbocommit.nix
+++ home/programs/turbocommit.nix
@@ -9,13 +9,16 @@ let
       owner = "dikkadev";
       repo = "turboCommit";
       rev = "v3.0.0";
-      sha256 = "0000000000000000000000000000000000000000000000000000";
+      sha256 = "EFRUCXZCvZWMVQRCes+f06Cl3cEgrwRY8bjS9u/Mtqw=";
     };
 
     cargoLock = {
       lockFile = "${src}/Cargo.lock";
     };
-    cargoSha256 = "0000000000000000000000000000000000000000000000000000";
+    cargoSha256 = "EFRUCXZCvZWMVQRCes+f06Cl3cEgrwRY8bjS9u/Mtqw=";
+
+    nativeBuildInputs = [ pkgs.pkg-config ];
+    buildInputs = [ pkgs.zlib pkgs.openssl ];
 
     doCheck = false;
   };
