7d7791c948f239aa064a05df13f5cde6ad5be88a
Gary Clark <gclark@xccelerated.io>
2026-02-28 17:30:00 +0100

    fix: patch common into explicit configs

--- home/common.nix
+++ home/common.nix
@@ -11,33 +11,34 @@ in
     description = "Root path for shared config files.";
   };
 
-  home.username = username;
-  home.homeDirectory = homedir;
-  # This is required and should not be changed once set
-  home.stateVersion = "23.11";
+  config = {
+    home.username = username;
+    home.homeDirectory = homedir;
+    # This is required and should not be changed once set
+    home.stateVersion = "23.11";
 
-  imports = [
-    ./programs/wezterm.nix
-    ./programs/starship.nix
-    ./programs/nushell.nix
-    ./programs/atuin.nix
-    ./programs/shell-tools.nix
-    ./programs/gitui.nix
-    ./programs/helix.nix
-    ./programs/yazi.nix
-    ./programs/git.nix
-    ./programs/rust.nix
-    ./programs/nix.nix
-    ./programs/lsps.nix
-    # ./programs/serie.nix
-    ./programs/general-applications.nix
-    ./programs/underlying-libs.nix
-    ./programs/gurk.nix
-    ./programs/codex.nix
-    ./programs/ios.nix
-    ./programs/nordvpn.nix
-    ./programs/nordpass.nix
-    # ./programs/neonctl.nix
-  ];
-
+    imports = [
+      ./programs/wezterm.nix
+      ./programs/starship.nix
+      ./programs/nushell.nix
+      ./programs/atuin.nix
+      ./programs/shell-tools.nix
+      ./programs/gitui.nix
+      ./programs/helix.nix
+      ./programs/yazi.nix
+      ./programs/git.nix
+      ./programs/rust.nix
+      ./programs/nix.nix
+      ./programs/lsps.nix
+      # ./programs/serie.nix
+      ./programs/general-applications.nix
+      ./programs/underlying-libs.nix
+      ./programs/gurk.nix
+      ./programs/codex.nix
+      ./programs/ios.nix
+      ./programs/nordvpn.nix
+      ./programs/nordpass.nix
+      # ./programs/neonctl.nix
+    ];
+  };
 }
